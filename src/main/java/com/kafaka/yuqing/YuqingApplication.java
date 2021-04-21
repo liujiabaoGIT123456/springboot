eef44f349ef02de56f833e6484948e234243ae08
刘佳宝 <834126420@qq.com>
2021-04-21 21:30:07 +0800

    数据库密码加密

--- src/main/java/com/kafaka/yuqing/YuqingApplication.java
+++ src/main/java/com/kafaka/yuqing/YuqingApplication.java
@@ -1,12 +1,14 @@
 package com.kafaka.yuqing;
 
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.kafaka.yuqing.dao")
+@EnableEncryptableProperties
 public class YuqingApplication {
     public static void main(String[] args) {
         SpringApplication.run(YuqingApplication.class, args);
