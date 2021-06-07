dd28221c6d832b0d8f303f4f4c56955ffd7f4bc6
刘佳宝 <834126420@qq.com>
2021-06-07 11:40:45 +0800

    yml

--- src/main/java/com/kafaka/yuqing/comment/InitProject.java
+++ src/main/java/com/kafaka/yuqing/comment/InitProject.java
@@ -15,13 +15,16 @@ public class InitProject implements ApplicationRunner {
     //获取项目端口号
     @Value("${spring.datasource.url}")
     private String servePrort;
+    @Value("${name}")
+    private String name;
+
     private final static Logger logger = LoggerFactory.getLogger(InitProject.class);
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
         logger.info("开始写入日志------------------------");
-        System.out.println("启动项目成功");
-        logger.info("项目端口号："+servePrort);
+        System.out.println("启动项目成功:"+servePrort);
+        logger.info("环境："+name);
         logger.info("项目初始化成功-----------------");
 
     }
--- src/main/java/com/kafaka/yuqing/controller/TestDBController.java
+++ src/main/java/com/kafaka/yuqing/controller/TestDBController.java
@@ -1,17 +1,14 @@
 package com.kafaka.yuqing.controller;
 
 
-import cn.hutool.json.JSONUtil;
+
 import com.kafaka.yuqing.dto.Test;
 import com.kafaka.yuqing.service.TestService;
 
-import com.yuqing.kafka.client.config.YuqingKfInitInfo;
-import com.yuqing.kafka.client.interfaces.YuqingKafkaHandlerMessage;
-import com.yuqing.kafka.client.service.YuqingKafkaService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +18,8 @@ import java.util.List;
 
 public class TestDBController {
     private final TestService testDBDao;
-    @Autowired
-    private RedisTemplate<String,String> redisTemplate;
+//    @Autowired
+//    private RedisTemplate<String,String> redisTemplate;
 
     public TestDBController(TestService testDBDao) {
         this.testDBDao = testDBDao;
@@ -32,7 +29,7 @@ public class TestDBController {
 
     //连接数据库测试
     @RequestMapping("/test")
-    public List<Test> test() {
+    public String test() {
 
         List<Test> test = testDBDao.test();
 //        Test test1 = new Test();
@@ -40,30 +37,30 @@ public class TestDBController {
 //        testDBDao.insert(test1);
         System.out.println(test);
         logger.info("test++++++++++");
-        return testDBDao.test();
+        return "刘佳宝爱张祝";
     }
 
-    //kafka测试
-    public static void main(String[] args) {
-        logger.info("test++++++++++");
-        YuqingKfInitInfo yuqingKfInitInfo=new YuqingKfInitInfo();
-        yuqingKfInitInfo.setUser("SDK1");
-        yuqingKfInitInfo.setPasswrod("111111");
-        yuqingKfInitInfo.setEnv("test");
-        YuqingKafkaHandlerMessage yuqingKafkaHandlerMessage=(a)->{
-            //获取所有数据
-            System.out.println(JSONUtil.parse(a).toString());
-        };
-        YuqingKafkaService.initService(yuqingKafkaHandlerMessage,yuqingKfInitInfo);
-
-    }
-
-    //redis测试
-    @RequestMapping("/redis")
-    public String redisTest(){
-        redisTemplate.opsForValue().set("test","redis测试成功");
-        return redisTemplate.opsForValue().get("test");
-    }
+//    //kafka测试
+//    public static void main(String[] args) {
+//        logger.info("test++++++++++");
+//        YuqingKfInitInfo yuqingKfInitInfo=new YuqingKfInitInfo();
+//        yuqingKfInitInfo.setUser("SDK1");
+//        yuqingKfInitInfo.setPasswrod("111111");
+//        yuqingKfInitInfo.setEnv("test");
+//        YuqingKafkaHandlerMessage yuqingKafkaHandlerMessage=(a)->{
+//            //获取所有数据
+//            System.out.println(JSONUtil.parse(a).toString());
+//        };
+//        YuqingKafkaService.initService(yuqingKafkaHandlerMessage,yuqingKfInitInfo);
+//
+//    }
+//
+//    //redis测试
+//    @RequestMapping("/redis")
+//    public String redisTest(){
+//        redisTemplate.opsForValue().set("test","redis测试成功");
+//        return redisTemplate.opsForValue().get("test");
+//    }
 
 
 }
